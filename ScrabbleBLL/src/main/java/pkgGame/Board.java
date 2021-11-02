1622457c3fe2fe136a997f684f6592c03854ab6f
Joyboy97 <84058420+Joyboy97@users.noreply.github.com>
2021-11-02 02:38:16 -0400

    finally

--- ScrabbleBLL/src/main/java/pkgGame/Board.java
+++ ScrabbleBLL/src/main/java/pkgGame/Board.java
@@ -18,10 +18,18 @@ public class Board {
 	 * @since Lab #6
 	 */
 	public Board() {
+		SetBonus();
+		puzzle = new Space[15][15];
+	
 		// TODO: Call SetBonus() to set the original bonus setup
 		// TODO: Create the default puzzle
 		// TODO: Loop through each place in the puzzle, insert a 'space' object, value
 		// the BonusSquare
+		for (int i =0; i<15;i++) {
+			for(int j =0; j<15; j++) {
+				puzzle[i][j]=new Space(findBonus(i,j));
+			}
+		}
 		// object if there's a bonus in that cell.
 	}
 
@@ -55,10 +63,54 @@ public class Board {
 	 * 
 	 * @author BRG
 	 * @version Lab #6
+	 * @param TripleWord 
 	 * @since Lab #6
 	 */
 	private void SetBonus() {
 		// TODO: Set the bonus squares. Use the default / class Scrabble settings.
+		
+		int b[][] =
+				  {{4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4,},
+				  {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0,},
+				  {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0,},
+				  {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1,},
+				  {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0,},
+				  {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0,},
+				 {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0,},
+				 {4, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 4,},
+				 {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0,},
+				 {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0,},
+				 {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0,},
+				  {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1,},
+				  {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0,},
+				 {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0,},
+				 {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4,}
+				  };
+		for (int i =0; i<15;i++) {
+			for(int j =0; j<15; j++) {
+				switch(b[i][j]) {
+				case 1: 
+					bonuses.add(new BonusSquare(i,j,eBonusType.DoubleLetter));
+					break;
+				case 2:
+					bonuses.add(new BonusSquare(i,j,eBonusType.TripleLetter));
+					break;
+				case 3:
+					bonuses.add(new BonusSquare(i,j,eBonusType.DoubleWord));
+					break;
+				case 4:
+					 bonuses.add(new BonusSquare(i,j,eBonusType.TripleWord));
+					 break;
+				case 5:
+					bonuses.add(new BonusSquare(i,j,eBonusType.Star));
+					break;
+				default:
+					bonuses.add(new BonusSquare(i,j,null));
+			
+				}
+			}
+		} 
 	}
+
 
 }
